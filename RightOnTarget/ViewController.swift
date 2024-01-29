87e698a17f3973291fc89729ee92b9f6177e259f
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-01-29 17:17:54 +0300

    Initial Commit

---
+++ RightOnTarget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RightOnTarget
+//
+//  Created by Andrei Shpartou on 29/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
