24f64f6c34c448aed9f88e5710b19dcfbcb5e7ba
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-02-06 20:05:31 +0300

    apply MVC pattern

--- RightOnTarget/ViewController.swift
+++ RightOnTarget/ViewController.swift
@@ -8,19 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet var slider: UISlider!
     @IBOutlet var label: UILabel!
     
-    // загаданное число
-    var number: Int = 0
-    // раунд
-    var round: Int = 0
-    // сумма очков за раунд
-    var points: Int = 0
-    
-    lazy var secondViewController: SecondViewController = getSecondViewController()
-    
+    var gameModel: Game!
+
     override func loadView() {
         super.loadView()
         print("loadView")
@@ -28,8 +20,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        gameModel = Game(startValue: 1, endValue: 50, rounds: 5)
+        updateLabelText(with: String(gameModel.currentSecretValue))
         print("viewDidLoad")
-        setupNewGame()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -51,59 +44,27 @@ class ViewController: UIViewController {
         super.viewDidDisappear(animated)
         print("ViewDidDisappear")
     }
-    
-    private func setupNewGame() {
-        self.number = Int.random(in: 1...50)
-        self.label.text = String(self.number)
-        self.round = 1
-    }
-    
+    // MARK: - View - Model interaction
     @IBAction func checkNumber() {
-        
-        let sliderValue = Int(self.slider.value.rounded())
-        if self.number > sliderValue {
-            self.points = self.points + 50 - self.number + sliderValue
-        } else if self.number < sliderValue {
-            self.points = self.points + 50 - sliderValue + self.number
+        let currentUserValue = Int(slider.value.rounded())
+        gameModel.calculateScore(with: currentUserValue)
+        if gameModel.isGameEnded {
+            showAlertWith(score: gameModel.score)
+            gameModel.restartGame()
         } else {
-            self.points += 50
+            gameModel.startNewRound()
         }
-        
-        self.round += 1
-        
-        if round == 6 {
-            
-            let alert = UIAlertController(
-                title: "Game over",
-                message: "You earn \(self.points) points",
-                preferredStyle: .alert)
-            
-            alert.addAction(UIAlertAction(title: "Play again!", style: .default, handler: nil))
-            
-            self.present(alert, animated: true, completion: nil)
-            self.points = 0
-            setupNewGame()
-            return
-        }
-        
-        self.number = Int.random(in: 1...50)
-        label.text = String(self.number)
-        
+        updateLabelText(with: String(gameModel.currentSecretValue))
     }
-
-    private func getSecondViewController() -> SecondViewController {
-        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let secondViewController = mainStoryboard.instantiateViewController(withIdentifier: "SecondViewController")
-        
-        return secondViewController as! SecondViewController
+    // MARK: - View updating
+    func updateLabelText(with newText: String) {
+        label.text = newText
     }
     
-//    @IBAction func showNextScreen() {
-//        self.present(secondViewController, animated: true, completion: nil)
-//    }
-    
-
-
-
+    func showAlertWith(score: Int) {
+        let alert = UIAlertController(title: "Game over", message: "Your score is \(score)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "restart game", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
