792e3b9f44fe579e2608164c1e3cb7095907a27c
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-02-09 12:06:02 +0300

    v 1.4 add choose game and guess game scenes

---
+++ RightOnTarget/Controller/ChooseGameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseGameViewController.swift
+//  RightOnTarget
+//
+//  Created by Andrei Shpartou on 09/02/2024.
+//
+
+import UIKit
+
+class ChooseGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
